c99540398802b9580b1f0eae4c029c3b4ae48335
ivnlsv <ivan.v.losev@gmail.com>
2025-01-06 20:16:50 +0300

    update

--- script.js
+++ script.js
@@ -1,47 +1,59 @@
 "use strict";
-
-function guessNumberBot(attempts, randomNum) {
-  const attempts = 10;
+function guessNumberBot() {
   let randomNum = Math.floor(Math.random() * 100) + 1;
+  const attempts = 10;
   function guessNumber() {
-    const userInput = prompt("Угадай число от 1 до 100");
+    const userInput = prompt(
+      `Угадай число от 1 до 100. Осталось попыток: ${attempts}`
+    );
+
     if (userInput === null) {
       alert("Игра окончена");
       return;
     }
+
     const userGuess = Number(userInput);
+
     if (isNaN(userGuess)) {
       alert("Введи число!");
       guessNumber();
     } else if (userGuess > randomNum) {
       attempts--;
-      alert(`Загаданное число меньше, осталось попыток: ${attempts}`);
-      guessNumber();
+      alert(`Загаданное число меньше. Осталось попыток: ${attempts}`);
+      if (attempts > 0) {
+        guessNumber();
+      } else {
+        alert("Попытки закончились, хотите сыграть еще?");
+        if (confirm("Начать новую игру?")) {
+          //  randomNum = Math.floor(Math.random() * 100) + 1;
+          //  attempts = 10;
+          guessNumber();
+        }
+      }
     } else if (userGuess < randomNum) {
       attempts--;
-      alert(`Загаданное число больше, осталось попыток: ${attempts}`);
-      guessNumber();
-    } else {
-      alert("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
-      if (confirm("Нажмите OK для новой игры.")) {
-        return guessNumber();
+      alert(`Загаданное число больше. Осталось попыток: ${attemptsLeft}`);
+      if (attempts > 0) {
+        guessNumber();
       } else {
-        alert("Игра окончена.");
-        return;
+        alert("Попытки закончились, хотите сыграть еще?");
+        if (confirm("Начать новую игру?")) {
+          //randomNum = Math.floor(Math.random() * 100) + 1;
+          //attempts = 10;
+          guessNumber();
+        }
       }
-    }
-    if (attempts <= 0) {
-      alert("Попытки закончились, хотите сыграть еще?");
-      if (confirm("Нажмите OK для новой игры.")) {
-        return guessNumber();
-      } else {
-        alert("Игра окончена.");
-        return;
+    } else {
+      alert("Поздравляю, Вы угадали!!!");
+      if (confirm("Хотели бы сыграть еще?")) {
+        //randomNum = Math.floor(Math.random() * 100) + 1;
+        //attempts = 10;
+        guessNumber();
       }
     }
   }
 
-  return guessNumber(attempts, randomNum);
+  guessNumber();
 }
 
 guessNumberBot();
