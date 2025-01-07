17996d74db44e0a9a0d1da085bcce56b50e97103
ivnlsv <ivan.v.losev@gmail.com>
2025-01-07 11:01:34 +0300

    Updated

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 "use strict";
 function guessNumberBot() {
   let randomNum = Math.floor(Math.random() * 100) + 1;
-  const attempts = 10;
-  function guessNumber() {
+
+  function guessNumber(attempts) {
     const userInput = prompt(
       `Угадай число от 1 до 100. Осталось попыток: ${attempts}`
     );
@@ -16,44 +16,40 @@ function guessNumberBot() {
 
     if (isNaN(userGuess)) {
       alert("Введи число!");
-      guessNumber();
+      guessNumber(attempts);
     } else if (userGuess > randomNum) {
       attempts--;
       alert(`Загаданное число меньше. Осталось попыток: ${attempts}`);
+
       if (attempts > 0) {
-        guessNumber();
+        guessNumber(attempts);
       } else {
         alert("Попытки закончились, хотите сыграть еще?");
         if (confirm("Начать новую игру?")) {
-          //  randomNum = Math.floor(Math.random() * 100) + 1;
-          //  attempts = 10;
-          guessNumber();
+          guessNumberBot();
         }
       }
     } else if (userGuess < randomNum) {
       attempts--;
-      alert(`Загаданное число больше. Осталось попыток: ${attemptsLeft}`);
+      alert(`Загаданное число больше. Осталось попыток: ${attempts}`);
+
       if (attempts > 0) {
-        guessNumber();
+        guessNumber(attempts);
       } else {
         alert("Попытки закончились, хотите сыграть еще?");
         if (confirm("Начать новую игру?")) {
-          //randomNum = Math.floor(Math.random() * 100) + 1;
-          //attempts = 10;
-          guessNumber();
+          guessNumberBot();
         }
       }
     } else {
       alert("Поздравляю, Вы угадали!!!");
       if (confirm("Хотели бы сыграть еще?")) {
-        //randomNum = Math.floor(Math.random() * 100) + 1;
-        //attempts = 10;
-        guessNumber();
+        guessNumberBot();
       }
     }
   }
 
-  guessNumber();
+  guessNumber(10);
 }
 
 guessNumberBot();
